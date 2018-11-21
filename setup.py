1e63692cd830907ac125287f147ffb3e188703af
Marius Gedminas <marius@gedmin.as>
2018-11-21 15:00:48 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import ast
+import os
+import re
+
+from setuptools import setup
+
+
+here = os.path.dirname(__file__)
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+metadata = {}
+with open(os.path.join(here, 'gitlab_jobs.py')) as f:
+    rx = re.compile('(__version__|__author__|__url__|__licence__) = (.*)')
+    for line in f:
+        m = rx.match(line)
+        if m:
+            metadata[m.group(1)] = ast.literal_eval(m.group(2))
+version = metadata['__version__']
+
+setup(
+    name='gitlab-jobs',
+    version=version,
+    author='Marius Gedminas',
+    author_email='marius@gedmin.as',
+    url='https://github.com/mgedmin/gitlab-jobs',
+    description='history of GitLab CI job running times',
+    long_description=long_description,
+    keywords='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='GPL',
+    python_requires=">=3.4",
+
+    py_modules=['gitlab_jobs'],
+    zip_safe=False,
+    install_requires=['python-gitlab'],
+    entry_points={
+        'console_scripts': [
+            'gitlab-jobs = gitlab_jobs:main',
+        ],
+    },
+)
